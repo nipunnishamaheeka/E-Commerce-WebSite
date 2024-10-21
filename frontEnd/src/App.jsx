a12ccdd6d36fbc7d5f5fbab4202ad69919aca098
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-21 08:15:13 +0530

    refactor routing configuration

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -14,7 +14,7 @@ import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
--- frontEnd/src/components/LetestCollection.jsx
+++ frontEnd/src/components/LetestCollection.jsx
@@ -1,11 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 function LetestCollection() {
   const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-  console.log(products);
-  return <div></div>;
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST "} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum magnam
+          corrupti eos cumque quam! Voluptate facilis nulla inventore nihil hic
+          incidunt quae harum eius cumque, quis provident molestias? Iste,
+          perspiciatis.
+        </p>
+      </div>
+      {/* {Rendering products} */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default LetestCollection;
