a2fbba1a3beeb77ef28544c6d412cf8cb57c769c
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-21 10:34:40 +0530

    add BestSeller component

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LetestCollection from '../components/LetestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
       <div>
       <Hero />
-      <LetestCollection/>
+      <LetestCollection />
+      <BestSeller/>
     </div>
   )
 }
