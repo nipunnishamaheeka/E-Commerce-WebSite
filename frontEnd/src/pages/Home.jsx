0481f161de7b9fe676c98390a4914773721a6848
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-21 10:43:22 +0530

    refactor BestSeller component and add OurPolicy component

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LetestCollection from '../components/LetestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
       <div>
       <Hero />
       <LetestCollection />
-      <BestSeller/>
+      <BestSeller />
+      <OurPolicy/>
     </div>
   )
 }
