d5fac7ebd4c0ced86827383b40363aa43506bf80
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-28 09:13:37 +0530

    cart component to display product details and quantity in the shopping cart

--- frontEnd/src/pages/Cart.jsx
+++ frontEnd/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
@@ -48,8 +49,14 @@ const Cart = () => {
                   <p className="text-xs sm:text-lg font-medium">
                     {productData.name}
                   </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>{currency}{productData.price}</p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{ item.size}</p>
+                  </div>
                 </div>
               </div>
+              <input className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number" min={1} defaultValue={item.quantity} />
+              <img className="w-4 mr-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt="" />
             </div>
           ) : (
             <div key={index} className="py-4 border-b text-gray-700">
